f20f41a97f788db3eb1791c88c534b3d9647d37c
Admin <Admin@DESKTOP-9L5JQOO>
2023-06-08 18:41:38 +0800

    Add project files.

---
+++ DATABASE2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            string ConnectionString = "Integrated Security=SSPI;" +
+     "Initial Catalog=EnrollmentDB;" +
+     "Data Source = DESKTOP-9L5JQOO\\SQLEXPRESS";
+
+
+
+            SqlConnection conn2 = new SqlConnection(ConnectionString);
+            conn2.Open();
+            using (SqlCommand cmd2 = new SqlCommand("insertNewStudent", conn2))
+            {
+                cmd2.CommandType = CommandType.StoredProcedure;
+
+
+
+                cmd2.Parameters.Add("@Firstname", SqlDbType.VarChar).Value = "Jan Mr TRC!";
+                cmd2.Parameters.Add("@Lastname", SqlDbType.VarChar).Value = "Camacho";
+
+                cmd2.ExecuteNonQuery();
+            }
+
+            
+        }
+    }
+}
